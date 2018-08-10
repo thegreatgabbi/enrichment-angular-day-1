fe37ef63580b861864b652ca90b162d19d64894b
thegreatgabbi <onethousand.words.88@gmail.com>
2018-08-10 21:15:26 +0800

    Validation working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AddressModel } from './model';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  addresses: AddressModel[] = [
+    {
+      name: "Fred Flintstone",
+      phone: "555-12345",
+      email: "fred@gmailcom"
+    },
+    {
+      name: "Barney rubble",
+      phone: "555-67890",
+      email: "barney@gmailcom"
+    },
+  ]
+
   title = 'day1-workshop';
+
+  submitForm(form) {
+    console.log("received", form);
+    this.addresses.push(form.value);
+    console.log(this.addresses);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form.component';
+import { AddressComponent } from './components/address.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    AddressComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
